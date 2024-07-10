20af14443381f83dc0876c6ed4cbe5a64ae348d5
Joha-will <johawilsonatu@icloud.com>
2024-07-10 23:12:50 +0100

    added functions and icons to change when the temperature changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const App = () => {
 
+  const [temperature, setTemperature] = useState(10);
+  const [temperatureColour, setTemperatureColour] = useState("");
+  const [temperatureIcon, setTemperatureIcon] = useState("");
+
+  const increaseTemperature = () => {
+    const newTemperature = temperature + 1;
+    setTemperature(newTemperature);
+    setTemperatureColour(newTemperature > 20? "hot" : newTemperature > 15? "neutral" : "blue");
+    setTemperatureIcon(newTemperature > temperature? "fa-solid fa-temperature-arrow-up" : "");
+    
+  }
+
+  const decreaseTemperature = () => {
+    const newTemperature = temperature - 1;
+    setTemperature(newTemperature);
+    setTemperatureColour(newTemperature > 20? "hot" : newTemperature > 15? "neutral" : "blue");
+    setTemperatureIcon(newTemperature < temperature? "fa-solid fa-temperature-arrow-down" : "");
+  }
+
   return (
+
+    <>
+    
+    <h1><i class={`icon-size  ${temperatureIcon}`}></i></h1>
+
     <div className="app-container">
 
       <div className="temperature-display-container">
-        <div className="temperature-display"> 10°C</div>
+        <div className={`temperature-display ${temperatureColour}`}> {temperature}°C</div>
       </div>
 
       <div className="button-container">
-        <button className="button">+</button>
-        <button className="button">-</button>
+        <button className="button" onClick={() => increaseTemperature()}>+</button>
+        <button className="button" onClick={() => decreaseTemperature()}>-</button>
       </div>
       
     </div>
+      
+      
+    </>
   );
 }
 
